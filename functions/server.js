d66908adc15db69671f27585064e3065675908b9
Farhana Aktar <farhanaaktar@live.co.uk>
2025-08-05 17:29:58 +0100

    mobile loading error

--- functions/server.js
+++ functions/server.js
@@ -96,7 +96,7 @@ app.get('/events', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.MY_PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
